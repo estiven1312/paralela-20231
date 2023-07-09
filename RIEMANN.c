177fd734ed8a3f013c708a26dd42af504593c409
estiven <santosestiven46@gmail.com>
2023-07-09 18:52:53 -0500

    mejorada version eliminando error de duplicados de lf

--- RIEMANN.c
+++ RIEMANN.c
@@ -12,7 +12,7 @@
 
 #define X0 4
 #define XN 10
-#define N 100000000
+#define N 1000
 //In this case we have to use f(x) = x^3, but the rieman algorithm uses any positive function
 
 double functionRiemann(double x){
@@ -24,20 +24,23 @@ double functionRiemann(double x){
 int main(int argc, char **argv)
 {
     int rank, size;
-    double b =(XN - X0)/(N*1.0d);
+    double b =(XN - X0)*1.0d/(N);
     double sum = 0.0d;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     double li = X0*1.0d + b*rank*N/size;
+
     double lf = X0*1.0d + b*(rank + 1)*N/size;
-    for(double i = li; i<lf; i= i + b){
+    
+    for(double i = li; i<lf-0.001; i = i + b){
         sum += functionRiemann(i)*b;
     } 
     double riemann_sum = 0.0d;
-
+    
     MPI_Reduce(&sum, &riemann_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     if(rank == 0){
+        printf("Interval b = %f\n",b);
         printf("The riemann sum of a function x³ is: %f", riemann_sum);
     }
     MPI_Finalize();
